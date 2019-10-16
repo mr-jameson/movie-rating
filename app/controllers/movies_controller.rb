1efbcb97c831cfd85b859124287c4a5b6baedf55
mr-jameson <elliot.jameson4@gmail.com>
2019-10-16 16:28:00 +1100

    added routes and controller methods:

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,24 @@
+class MoviesController < ApplicationController
+    def index
+        @movies = Movie.all
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html4
+  # resources :movies
+  get "/movies", to: "movies#index", as: "movies"
+  get "/movies/new", to: "movies#new", as: "new_movie"
+  post "/movies", to: "movies#create"
+  get "/movies/:id", to: "movies#show", as: "movie"
+  get "/movies/:id/edit", to: "movies#edit", as: "edit_movie"
+  put "/movies/:id", to: "movies#update"
+  delete "/movies/:id", to: "movies#destroy"
 end
