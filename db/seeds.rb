1f91d6acb29d388d03fff30b70826d8193b5edab
mr-jameson <elliot.jameson4@gmail.com>
2019-10-16 15:38:31 +1100

    populated databases

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,15 +10,34 @@ for i in 1..40
     Actor.create(
         name: Faker::Name.name,
         birthdate: Faker::Date.birthday(min_age: 12, max_age: 70),
-        nationality: Faker::Nation.nationalitys
+        nationality: Faker::Nation.nationality
     )
+    puts "Created #{i} actors"
 end
+
+actor_ids = Actor.pluck(:id)
+puts "Grabbed actor ids"
 
 for i in 1..20
     m = Movie.create(
         title: Faker::Book.title,
-        rating: rand(1..10)
-        description: Faker::Lorem.paragraph
+        rating: rand(1..10),
+        description: Faker::Lorem.paragraph,
         image: Faker::LoremPixel.image
     )
+
+    puts "Created movie #{m.title}"
+
+    movie_actors = actor_ids.sample(rand(1..5))
+
+    for y in movie_actors
+        actor = Actor.find(y)
+        # m.actors.push(actor)
+        ActorsMovie.create(
+            movie_id: m.id,
+            actor_id: actor.id,
+            character: Faker::Superhero.name
+        )
+        puts "Actor #{actor.name}"
+    end
 end
